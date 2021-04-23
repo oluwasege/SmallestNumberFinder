d057abf2288fa9ad90caaa77927545e0991f2894
Oluwasegun <akinpelu53@gmail.com>
2021-04-23 14:43:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SmallestNumberFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! ");
+            Console.WriteLine("This is a program to find the smallest of three numbers you Enter");
+            Console.WriteLine("Please Enter the first number");
+            int fnumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter the second number");
+            int snumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter the third number");
+            int tnumber = int.Parse(Console.ReadLine());
+            int[] number = {fnumber,snumber,tnumber };
+            int min = number.Min();
+            Console.WriteLine($"The minimum smallest number is {min}");
+
+        }
+    }
+}
